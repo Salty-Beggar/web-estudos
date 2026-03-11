44aeee62379acaad653ffc46e9c178401ef6788d
Salty-Beggar <ivolijuguedes@gmail.com>
2026-03-11 07:54:06 -0300

    Implementação parcial salvar + ajustes

--- prototype/script.js
+++ prototype/script.js
@@ -47,6 +47,7 @@ const classificacoes = [
 const postagens = [
     {
         nome: 'Prova de História',
+        tipo: 'Prova',
         usuario: 'Lucas Andrade da Silva',
         cargo: 'Professor',
         classificacoes: [
@@ -65,6 +66,7 @@ const postagens = [
     },
     {
         nome: 'Maus: Desenhos fofos com história trágica',
+        tipo: 'Artigo',
         usuario: 'milly_103',
         cargo: 'Aluna',
         classificacoes: [
@@ -88,6 +90,7 @@ const postagens = [
     },
     {
         nome: 'França: Tomada da bastilha',
+        tipo: 'Artigo',
         usuario: 'Luiza Carla',
         cargo: 'Hobbyista',
         classificacoes: [
@@ -101,6 +104,7 @@ const postagens = [
     },
     {
         nome: 'Guerra Mundial',
+        tipo: 'Vídeo',
         usuario: 'Mario Mendonça',
         cargo: 'Aluno',
         classificacoes: [
@@ -124,6 +128,7 @@ const postagens = [
     },
     {
         nome: 'Como escrever redações: inicio',
+        tipo: 'Artigo',
         usuario: 'Lucas Andrade da Silva',
         cargo: 'Professor',
         classificacoes: [
@@ -137,6 +142,7 @@ const postagens = [
     },
     {
         nome: 'Gente oq é uma celula????',
+        tipo: 'Post Social',
         usuario: 'Pedro_Gamer',
         cargo: 'Aluno',
         classificacoes: [
@@ -155,6 +161,7 @@ const postagens = [
     },
     {
         nome: 'Questão ENEM Invertebrados',
+        tipo: 'Atividade',
         usuario: 'Lucas Andrade da Silva',
         cargo: 'Professor',
         classificacoes: [
@@ -176,13 +183,14 @@ const postagens = [
 for (let i = 0; i < postagens.length; i++) {
     for (let j = 0; j < postagens[i].classificacoes.length; j++) {
         postagens[i].classificacoes[j].votoUser = 0;
+        postagens[i].classificacoes[j].salvo = false;
     }
 }
 
 const postDiv = `<div id="feed-container-main">
                     <div id="feed-container" data-owner="feed-container">
                         <section class="feed-container-data">
-                            <h2 class="container-tittle">:nome</h2><br>
+                            <h2 class="container-tittle">:nome</h2><button id='salvar_post_:indice'></button><br>
                             <p class="container-type-user">Usuário: :usuario (:cargo)</p></br>
                             <p class="container-date">Data de postagem: 20/06/2026</p></br>
                             <p class="container-rating">
@@ -198,7 +206,7 @@ const postClassDiv = `:nome:
                                 Dificuldade: :dificuldade
                             </li>
                             <li>
-                                Votos: :votos <button class='voto_mais_:indice_:class'>+</button><button class='voto_menos_:indice_:class'>-</button>
+                                Votos: :votos <button class='voto_mais_:indice_:class :mais_selecionado'>+</button><button class='voto_menos_:indice_:class :menos_selecionado'>-</button>
                             </li>
                         </ul>`
 
@@ -212,7 +220,7 @@ function gerarPosts() {
                 a.classificacoes.forEach(element => {
                     console.log(element.class, i);
                     if (element.class == i) {
-                        aValue *= element.votos;
+                        aValue *= element.votos + element.votoUser*3;
                     }
                 });
             }
@@ -221,7 +229,7 @@ function gerarPosts() {
         for (let i = 0; i < document.getElementsByClassName('class_check').length; i++) {
             if ($id('class_check_'+i).checked) {
                 b.classificacoes.forEach(element => {
-                    if (element.class == i) bValue *= element.votos;
+                    if (element.class == i) bValue *= element.votos + element.votoUser*3;
                 });
             }
         }
@@ -240,9 +248,11 @@ function gerarPosts() {
             let strAtual = postClassDiv;
             strAtual = strAtual.replace(':nome', classificacoes[classificacaoAtual.class]);
             strAtual = strAtual.replace(':dificuldade', classificacaoAtual.rating);
-            strAtual = strAtual.replace(':votos', classificacaoAtual.votos + classificacaoAtual.votoUser);
-            strAtual = strAtual.replace(':indice', i);
-            strAtual = strAtual.replace(':class', classificacaoAtual.class);
+            strAtual = strAtual.replace(':votos', classificacaoAtual.votos + classificacaoAtual.votoUser*3);
+            strAtual = strAtual.replaceAll(':indice', i);
+            strAtual = strAtual.replaceAll(':class', classificacaoAtual.class);
+            if (classificacaoAtual.votoUser == 1) strAtual = strAtual.replace(':mais_selecionado', 'selecionado');
+            else if (classificacaoAtual.votoUser == -1) strAtual = strAtual.replace(':menos_selecionado', 'selecionado');
             classificacoesString += strAtual;
         }
 
@@ -250,6 +260,7 @@ function gerarPosts() {
         postDivAtual = postDivAtual.replace(':nome', postagemAtual.nome);
         postDivAtual = postDivAtual.replace(':usuario', postagemAtual.usuario);
         postDivAtual = postDivAtual.replace(':cargo', postagemAtual.cargo);
+        postDivAtual = postDivAtual.replace(':indice', i);
         postDivAtual = postDivAtual.replace(':classificacoes', classificacoesString);
 
         feedHeader.innerHTML += postDivAtual;
@@ -258,6 +269,7 @@ function gerarPosts() {
     for (let i = 0; i < postagens.length; i++) {
         const postagemAtual = postagens[i];
         const classificacoesPostagem = postagemAtual.classificacoes;
+
         for (let j = 0; j < classificacoesPostagem.length; j++) {
             const classificacaoAtual = classificacoesPostagem[j];
 
@@ -265,17 +277,23 @@ function gerarPosts() {
             const classificacaoVotoMenosClass = 'voto_menos_'+i+'_'+classificacaoAtual.class;
 
             const votoMaisDiv = document.getElementsByClassName(classificacaoVotoMaisClass)[0];
-            const votoMenosDiv = document.getElementsByClassName(classificacaoVotoMenosClass)[1];
+            const votoMenosDiv = document.getElementsByClassName(classificacaoVotoMenosClass)[0];
 
             votoMaisDiv.addEventListener('click', function() {
                 if (classificacaoAtual.votoUser != 1) classificacaoAtual.votoUser = 1;
                 else classificacaoAtual.votoUser = 0;
+                gerarPosts();
             });
             votoMenosDiv.addEventListener('click', function() {
                 if (classificacaoAtual.votoUser != -1) classificacaoAtual.votoUser = -1;
                 else classificacaoAtual.votoUser = 0;
+                gerarPosts();
             });
         }
+
+        const botaoSalvar = $id('salvar_post_'+i);
+        botaoSalvar.addEventListener();
+        // RIGHT_NOW: Continuar criando o event listener.
     }
 }
 
