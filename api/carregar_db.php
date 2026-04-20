f3259cd33fc50b1da0124d2dab97f94039884037
SaltyBeggar <saltybeggar@gmail.com>
2026-04-20 18:09:15 -0300

    Implementações iniciais e DB

--- api/carregar_db.php
+++ api/carregar_db.php
@@ -1,2 +1,19 @@
 <?php
 
+class DB {
+    private static $pdo;
+
+    public static function conectarBanco() {
+        self::$pdo = new PDO("mariadb:host=localhost;dbname=web_estudos", "root", "");
+    }
+
+    public static function raw(string $sql, Array $params, ?string $model) {
+        $comando = self::$pdo->prepare($sql, $params);
+        if (!empty($model)) $comando->setFetchMode(PDO::FETCH_CLASS, $model);
+        $comando->execute();
+        if (!empty($model)) return $comando->fetchAll();
+        return true;
+    }
+}
+
+DB::conectarBanco();
