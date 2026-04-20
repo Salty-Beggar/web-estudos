edd4c6d02d6e6d0daba241cba87231d755b12ec5
Davi Rodrigues de Almeida <davirralmeida@gmail.com>
2026-04-20 18:36:50 -0300

    mudei quase nada

--- public/js/components/sidebar.js
+++ public/js/components/sidebar.js
@@ -1,3 +1,50 @@
-export function render_sidebar(user_friends, user_servers, user_groups, user_courses, ){
+export async function create_sidebar(user_id){
+    const sidebar = document.createElement("aside");
+    const data_objects = await Promise.all([ 
+        fetch_servers(user_id), 
+        fetch_courses(user_id), 
+        fetch_friends(user_id), 
+        fetch_groups(user_id), 
+        fetch_chats(user_id)
+    ]);
 
+    data_objects.forEach((object) => {
+        if(Object.length(object) == 0){
+            const section = document.createElement("section"); 
+            const h2 = document.createElement("h2"); 
+            const p = document.createElement("p");
+
+        }
+    });
+
+}
+
+async function fetch_servers(user_id){
+    const data = await fetch(`/KnowledgeHub/api/servers/${user_id}`);  
+    const response = await data.json(); 
+    return response??[];
+}
+
+async function fetch_courses(user_id){
+    const data = await fetch(`/KnowledgeHub/api/courses/${user_id}`);  
+    const response = await data.json(); 
+    return response??[];
+}
+
+async function fetch_friends(user_id){
+    const data = await fetch(`/KnowledgeHub/api/friends/${user_id}`);  
+    const response = await data.json(); 
+    return response??[];
+}
+
+async function fetch_groups(user_id){
+    const data = await fetch(`/KnowledgeHub/api/groups/${user_id}`);  
+    const response = await data.json(); 
+    return response??[];
+}
+
+async function fetch_chats(user_id){
+    const data = await fetch(`/KnowledgeHub/api/chats/${user_id}`);  
+    const response = await data.json(); 
+    return response??[];
 }
--- public/js/pages/home.js
+++ public/js/pages/home.js
@@ -1,11 +1,14 @@
 import { render_header } from '/KnowledgeHub/public/js/components/header.js';
+import { render_sidebar } from '/KnowledgeHub/public/js/components/sidebar.js';
 import { get_current_user } from '/KnowledgeHub/public/js/app/usuarioAtual.js';
 import '/KnowledgeHub/public/js/app/usuarioAtual.js';
 
 export async function load_home_page(param){
     const user = get_current_user();
     const header = document.createElement('header');
-    // header.innerHTML = "<h1>ola mundo</h1>";
+    const sidebar = document.createElement('section');
     header.innerHTML = render_header(user);
+    sidebar.innerHTML = await render_sidebar();
     root.appendChild(header);
+    root.appendChild(sidebar);
 }
