d7ea7b11f30c0df2b83cf6147a309af8d65cfa63
SaltyBeggar <saltybeggar@gmail.com>
2026-04-25 17:34:56 -0300

    Implementação  de router, models e controllers inicial

--- api/model/Model.php
+++ api/model/Model.php
@@ -1,22 +1,48 @@
 <?php
 
-<<<<<<< HEAD
-abstract class Model implements JsonSerializable {
-    protected $tabela;
 
-    public function save() {
 
-=======
-public class Model implements JsonSerializable {
-    protected $table;
-    public function __construct() {
-        
-    }
+abstract class Model implements \JsonSerializable {
+    protected static $nome;
+    protected static $tabela;
+    protected static $relacoes;
+    protected static $atributos;
 
     public function fill($data) {
         foreach ($data as $key => $value) {
             $this->$key = $value;
         }
->>>>>>> 7d4a158 (Parallel changes)
     }
+
+    public function jsonSerialize() {
+
+    }
+
+    public static function fetch() {
+        if (empty(static::$nome)) static::$nome = substr(static::$tabela, -1);
+        static::$atributos = DB::tabelaColunas(static::$tabela);
+    }
+
+    #region DB functions
+
+    static public function select(String|Array $atributos = '*', String $sqlExtra = '', Array $params = []) {
+        if (is_array($atributos)) $atributos = implode(', ', $atributos);
+        $sql = "SELECT ? FROM ? {$sqlExtra}";
+        $paramsFinal = [$atributos, static::$tabela, ...$params];
+        return DB::query($sql, $paramsFinal, static::class);
+    }
+
+    static public function insert(Array $atributos, String $sqlExtra, Array $params) {
+        $insertString = '';
+        $valuesString = '';
+        foreach ($atributos as $chave => $valor) {
+            $insertString .= $chave.',';
+            $valuesString .= $valor.',';
+        }
+        $sql = "INSERT INTO ? (?) VALUES (?) {$sqlExtra}";
+        $paramsFinal = [static::$tabela, $insertString, $valuesString, ...$params];
+        return DB::executar($sql, $paramsFinal);
+    }
+
+    #endregion
 }
