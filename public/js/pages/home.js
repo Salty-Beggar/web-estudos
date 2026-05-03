bfe645f83d6946775ef5480fdbaa6a2521eb4d0e
Davi Rodrigues de Almeida <davirralmeida@gmail.com>
2026-05-03 14:10:47 -0300

    criado os posts do feed e o inicial pois nao tenho acesso ao banco ainda

--- public/js/pages/home.js
+++ public/js/pages/home.js
@@ -1,6 +1,7 @@
 import { render_barra_lateral } from '/js/components/sidebar.js';
 import { renderizar_cabecalho } from '/js/components/header.js';
 import { get_current_user } from '/js/app/usuario_atual.js';
+import { criar_post } from '/js/components/post.js';
 import '/js/app/usuario_atual.js';
 
 export async function load_home_page(param){
@@ -11,7 +12,7 @@ export async function load_home_page(param){
     root.appendChild(sidebar)
     root.appendChild(header);
     trocar_barra_lateral();
-    
+    await carregar_conteudo()
 }
 function trocar_barra_lateral(){
     const botao_menu = document.getElementById("botao_menu");
@@ -21,3 +22,21 @@ function trocar_barra_lateral(){
         root.classList.toggle("barra_lateral_fechada");
     });
 }
+async function carregar_conteudo(){
+    const corpo = document.getElementById("corpo_container")
+    const posts = await buscar_posts();
+    if(posts.length === 0){
+        const mensagem = document.createElement("p");
+        mensagem.textContent = "Nenhum post encontrado.";
+        corpo.appendChild(mensagem);
+    }
+    posts.forEach(post => {
+        const post_elemento = criar_post(post)//sera um item html
+        corpo.appendChild(post_elemento)
+    })
+}
+async function buscar_posts(){
+    const resposta = await fetch("http://localhost:3000/posts",{ method:"GET"});
+    const dados = await resposta.json();
+    return dados ?? [];
+} 
