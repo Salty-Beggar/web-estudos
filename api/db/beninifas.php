0ee1be95ce197b6cc92672d1cd465ce84d290a14
Salty-Beggar <ivolijuguedes@gmail.com>
2026-06-12 09:05:49 -0300

    Implementação quase final de migrations

--- api/db/beninifas.php
+++ api/db/beninifas.php
@@ -1,9 +1,32 @@
+#!php
+
 <?php
 
 $pdo = new PDO("mysql:host=db;dbname=web_estudos", "root", "1234");
-$comando = $argv[0];
+$comando = $argv[1];
+$status = null;
+$output = null;
 
-switch ($comando[0]) {
+switch ($comando) {
     case 'migrate':
-
+        $sql = $pdo->prepare('SELECT max(id) as max_id FROM events');
+        $sql->execute();
+        $sql->setFetchMode(PDO::FETCH_ASSOC);
+        $migration = $sql->fetch()['max_id'] ?? 0;
+        $directory = scandir('/srv/events');
+        array_splice($directory, 0, 2);
+        $directory = array_map(function($item) {
+            return [
+                'file' => $item, 
+                'tags' => explode('_', $item)
+            ];
+        }, $directory);
+        $directory = array_filter($directory, function($item) {
+            if (strlen($item['tags'][0]) > 2) return false;
+            return true;
+        });
+        var_dump($directory);
+        break;
 }
+
+return 1;
--- beninifas.php
+++ beninifas.php
@@ -3,11 +3,18 @@
 $configStr = file_get_contents('./beninifas.json');
 $config = json_decode($configStr);
 
-$comando = $argv[0];
+$comando = $argv[1];
 $status = null;
 $output = null;
 
 switch ($comando) {
     case 'migrate':
-        exec("docker exec -it {$config['containers']['db']} /srv/events/beninifaz.py migrate");
+        exec("docker exec -it {$config->containers->db} php /srv/beninifas.php migrate", $output, $status);
+        $output = implode("\n", $output);
+        break;
+    default:
+        $output = "Comando {$comando} não encontrado!";
+        break;
 }
+
+print_r($output);
