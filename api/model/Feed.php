a8dba4ec90cc0378240ac38c96f9061403265062
Salty-Beggar <ivolijuguedes@gmail.com>
2026-06-17 11:03:21 -0300

    Inheritance initial touches

--- api/model/Feed.php
+++ api/model/Feed.php
@@ -2,15 +2,10 @@
 
 require_once 'Model.php';
 
-class Usuario extends Model {
-    static protected $tabela = 'usuarios';
+class Feed extends Model {
+    static protected $tabela = 'feeds';
     static protected $atributos = [];
-    static protected $oneToMany = [
-        'feeds' => ['Feed', 'usuario_id']
-    ];
     static protected $manyToMany = [
         'categorias' => ['Categoria', 'posts_categorias', 'feed_id', 'categoria_id']
     ];
 }
-
-Usuario::fetch();
